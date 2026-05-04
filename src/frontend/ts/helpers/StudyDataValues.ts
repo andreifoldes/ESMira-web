664fde3be6edff4144425eb8b43d643f4e2aa5c8
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2026-05-04 13:50:20 +0200

    added usageProtocol index entries

--- src/frontend/ts/helpers/StudyDataValues.ts
+++ src/frontend/ts/helpers/StudyDataValues.ts
@@ -1,31 +1,33 @@
-import {Study} from "../data/study/Study";
-import {Questionnaire} from "../data/study/Questionnaire";
-import {Input} from "../data/study/Input";
+import { Study } from "../data/study/Study";
+import { Questionnaire } from "../data/study/Questionnaire";
+import { Input } from "../data/study/Input";
 
 function setResponseTypeValues(input: Input, variables: string[]): void {
 	const name = input.name.get()
-	switch(input.responseType.get()) {
+	switch (input.responseType.get()) {
 		case "text":
 			return
 		case "app_usage":
-			variables.push(name+"~usageTimeYesterday")
-			variables.push(name+"~usageCountYesterday")
-			variables.push(name+"~usageTimeToday")
-			variables.push(name+"~usageCountToday")
+			variables.push(name + "~usageTimeYesterday")
+			variables.push(name + "~usageCountYesterday")
+			variables.push(name + "~usageProtocolYesterday")
+			variables.push(name + "~usageTimeToday")
+			variables.push(name + "~usageCountToday")
+			variables.push(name + "~usageProtocolToday")
 			variables.push(name);
 			return
 		case "bluetooth_devices":
-			variables.push(name+"~devices")
+			variables.push(name + "~devices")
 			variables.push(name)
 			return
 		case "dynamic_input":
-			variables.push(name+"~index")
+			variables.push(name + "~index")
 			variables.push(name)
 			return
 		case "list_multiple":
 			variables.push(name)
 			input.listChoices.get()
-			for(let i = 1; i <= input.listChoices.get().length; i++) {
+			for (let i = 1; i <= input.listChoices.get().length; i++) {
 				variables.push(name + "~" + i.toString())
 			}
 			return
@@ -38,27 +40,27 @@ function setResponseTypeValues(input: Input, variables: string[]): void {
 export const StudyDataValues = {
 	getQuestionnaireVariableNames(questionnaire: Questionnaire): string[] {
 		const variables: string[] = []
-		for(const page of questionnaire.pages.get()) {
-			for(const input of page.inputs.get()) {
+		for (const page of questionnaire.pages.get()) {
+			for (const input of page.inputs.get()) {
 				variables.push(input.name.get())
 			}
 		}
-		
-		for(const sumScore of questionnaire.sumScores.get()) {
+
+		for (const sumScore of questionnaire.sumScores.get()) {
 			variables.push(sumScore.name.get());
 		}
-		
+
 		return variables
 	},
-	
+
 	getQuestionnaireVariables(questionnaire: Questionnaire): string[] {
 		const variables: string[] = []
-		for(const page of questionnaire.pages.get()) {
-			for(const input of page.inputs.get()) {
+		for (const page of questionnaire.pages.get()) {
+			for (const input of page.inputs.get()) {
 				setResponseTypeValues(input, variables)
 			}
 		}
-		
+
 		questionnaire.sumScores.get().forEach((sumScore) => {
 			variables.push(sumScore.name.get());
 		})
@@ -66,17 +68,17 @@ export const StudyDataValues = {
 		questionnaire.virtualInputs.get().forEach((virtualInput) => {
 			variables.push(virtualInput.get())
 		})
-		
+
 		return variables
 	},
-	
+
 	getStudyVariables(study: Study): string[] {
 		let variables: string[] = [];
 		let questionnaires = study.questionnaires.get();
 		questionnaires.forEach((questionnaire) => {
 			variables = variables.concat(this.getQuestionnaireVariables(questionnaire))
 		})
-		
+
 		return variables;
 	}
 }
