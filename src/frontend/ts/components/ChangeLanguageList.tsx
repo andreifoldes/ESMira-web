6383ac7242b67814e43a55cb9b0de835ce8fc3a9
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2026-05-11 14:03:52 +0200

    sorted language list by displayed language name

--- src/frontend/ts/components/ChangeLanguageList.tsx
+++ src/frontend/ts/components/ChangeLanguageList.tsx
@@ -1,44 +1,53 @@
-import m, {Vnode} from "mithril";
-import {Lang} from "../singletons/Lang";
-import {BindObservable, OnBeforeChangeTransformer} from "./BindObservable";
-import {TranslatableRootInterface} from "../observable/interfaces/TranslatableRootInterface";
-import {BtnTrash} from "./Buttons";
+import m, { Vnode } from "mithril";
+import { Lang } from "../singletons/Lang";
+import { BindObservable, OnBeforeChangeTransformer } from "./BindObservable";
+import { TranslatableRootInterface } from "../observable/interfaces/TranslatableRootInterface";
+import { BtnTrash } from "./Buttons";
 
 export class ChangeLanguageList {
 	public readonly promise: Promise<void>
 	private readonly langCodeNames: { langName: string, langCode: string }[] = []
 	private readonly getTranslationRoot: () => TranslatableRootInterface
-	
+
 	constructor(getTranslationRoot: () => TranslatableRootInterface) {
 		this.getTranslationRoot = getTranslationRoot
 		this.promise = import(`../../langCodes/${Lang.code}.json`)
-			.then(({default: data}) => data)
+			.then(({ default: data }) => data)
 			.then((langCollection: Record<string, string>) => {
-				for(let code in langCollection) {
-					this.langCodeNames.push({langName: langCollection[code], langCode: code})
+				for (let code in langCollection) {
+					this.langCodeNames.push({ langName: langCollection[code], langCode: code })
 				}
+				this.langCodeNames.sort(function (a, b) {
+					if (a.langName < b.langName) {
+						return -1
+					}
+					if (a.langName > b.langName) {
+						return 1
+					}
+					return 0
+				})
 			})
 	}
-	
+
 	private addLang(translationRoot: TranslatableRootInterface, e: InputEvent): void {
 		const element = e.target as HTMLSelectElement
 		const langCode = element.value
-		
+
 		translationRoot.langCodes.push(langCode)
 		translationRoot.addLanguage(langCode)
 		element.selectedIndex = 0
 		console.log(translationRoot, langCode)
 	}
-	
+
 	private removeLang(translationRoot: TranslatableRootInterface, langCode: string): void {
 		const index = translationRoot.langCodes.indexOf(langCode)
 		translationRoot.langCodes.remove(index)
 		translationRoot.removeLanguage(langCode)
-		
-		if(langCode === translationRoot.defaultLang.get())
+
+		if (langCode === translationRoot.defaultLang.get())
 			translationRoot.defaultLang.set(translationRoot.langCodes.get()[0].get() ?? "unknown")
 	}
-	
+
 	public getView(): Vnode<any, any> {
 		const translationRoot = this.getTranslationRoot()
 		const langCodes = translationRoot.langCodes.get()
@@ -46,15 +55,15 @@ export class ChangeLanguageList {
 			<div class="vertical hAlignStart">
 				{langCodes.map((langCode) =>
 					<div>
-						{ langCodes.length > 1 &&
+						{langCodes.length > 1 &&
 							BtnTrash(this.removeLang.bind(this, translationRoot, langCode.get()))
 						}
-						
+
 						<label class="middle">
 							<small>{Lang.get("language")}</small>
-							<select {... BindObservable(langCode, new OnBeforeChangeTransformer<string>(langCode, (before, after) => {
+							<select {...BindObservable(langCode, new OnBeforeChangeTransformer<string>(langCode, (before, after) => {
 								translationRoot.renameLanguage(before, after)
-								if(translationRoot.defaultLang.get() == before)
+								if (translationRoot.defaultLang.get() == before)
 									translationRoot.defaultLang.set(after)
 								return after
 							}))}>
@@ -63,19 +72,19 @@ export class ChangeLanguageList {
 							</select>
 						</label>
 						<label>
-							<input type="radio" name="default_language" checked={translationRoot.defaultLang.get() == langCode.get()} onchange={function(this:HTMLInputElement) {
-								if(this.checked)
+							<input type="radio" name="default_language" checked={translationRoot.defaultLang.get() == langCode.get()} onchange={function (this: HTMLInputElement) {
+								if (this.checked)
 									translationRoot.defaultLang.set(langCode.get())
-							}}/>
+							}} />
 							<span>{Lang.get("default_language")}</span>
 						</label>
-					
+
 					</div>
 				)}
 				<div class="spacingTop">
 					<label class="middle spacingLeft">
 						<small>{Lang.get("language")}</small>
-						
+
 						<select class="smallText" onchange={this.addLang.bind(this, translationRoot)}>
 							<option>{Lang.get("select_to_add")}</option>
 							{this.langCodeNames.map((entry) => <option value={entry.langCode}>{entry.langName}</option>)}
@@ -84,6 +93,6 @@ export class ChangeLanguageList {
 				</div>
 			</div>
 		</div>
-		
+
 	}
 }
